dcc3b7e89f268339289f816211cc57a65690305b
Rahul Gupta <rahulgupta.image@gmail.com>
2017-06-19 11:43:45 -0400

    inital summary
    initial commit

---
+++ src/main/java/com/rahul/stuff/App.java
@@ -0,0 +1,13 @@
+package com.rahul.stuff;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
